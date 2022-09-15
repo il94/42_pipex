5a8a6db977615ee2aae0a998b7c3230a2e2ae2c5
il94 <ilyes.landolsi@outlook.fr>
2022-09-15 22:52:14 +0200

    libft && tests

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ilandols <ilyes@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 16:54:06 by ilandols          #+#    #+#             */
-/*   Updated: 2022/09/14 19:10:28 by ilandols         ###   ########.fr       */
+/*   Updated: 2022/09/15 19:29:02 by ilandols         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,53 +15,73 @@
 void	pipex(t_path *commands, int cmd_count, char **av, char **envp)
 {
 	int	i;
-	int	fds[2];
 	char buf;
-	// int	*pipes;
-
-	// fds[0] = 0;
-	// fds[1] = open(av[cmd_count + 1], O_CREAT);
-	// if (fds[0] < 0 || fds[1] < 0)
-	// {
-	// 	close(fds[0]);
-	// 	close(fds[1]);
-	// 	free_struct_and_exit(commands, cmd_count, "File opening failed\n");
-	// }
-	// pipes = malloc(cmd_count * sizeof(int));
-	// if (!pipes)
-	// 	free_struct_and_exit(commands, cmd_count, "Malloc failed\n");	
-	if (pipe(fds) < 0)
-		ft_print_exit("Pipe failed\n");
-	commands[0].pid = fork();
-	if (commands[0].pid == 0)
-	{
-		close(fds[1]);
-		while (read(fds[0], &buf, 1) > 0)
-			write(1, &buf, 1);
-		write(1, "\n", 1);	
-		close(fds[0]);
-		exit(0);
-	}
-	else
-	{
-		close(fds[0]);
-		write(fds[1], "TEST\n", 5);
-		close(fds[1]);
-		wait(NULL);
-		exit(0);
-	}
-
-	i = 0;
+	int pid;
+	int infile;
+	int infile_dup;
+	int outfile;
+	int outfile_dup;
+	// int *pipes;
+	int pipes[2];
 	
-	while (i < cmd_count)
+	infile = open(av[1], O_WRONLY);
+	outfile = open(av[cmd_count + 2], O_WRONLY | O_CREAT);
+	// pipes = malloc(cmd_count * 2 * sizeof(int));
+	pipe(pipes);
+	i = 0;
+	// while (i < 2)
 	{
-		commands[i].pid = fork();
-		if (commands[i].pid < 0)
-			free_struct_and_exit(commands, cmd_count, "Fork failed\n");
-		else if (commands[i].pid > 0)
-			waitpid(commands[i].pid, NULL, 0);
+		pid = fork();
+		if (pid == 0)
+		{
+			dup2(infile, 0);
+			dup2(pipes[1], 1);
+			close(pipes[0]);
+			execve(commands[i].path, &commands[i].path, envp);
+			
+		}
 		else
-			execve(commands[i].path, commands[i].args, envp);
+		{
+			// close(pipes[1]);
+			waitpid(pid, NULL, 0);
+		}
 		i++;
 	}
+		pid = fork();
+		if (pid == 0)
+		{
+			dup2(pipes[0], 0);
+			dup2(outfile, 1);
+			close(pipes[0]);
+			execve(commands[i].path, &commands[i].path, envp);
+			
+		}
+		else
+		{
+			// close(pipes[1]);
+			waitpid(pid, NULL, 0);
+		}
+
+
+
+
+
+	
+	// i = 0;
+	// while (i < cmd_count)
+	// {
+	// 	commands[i].pid = fork();
+	// 	if (commands[i].pid < 0)
+	// 		free_struct_and_exit(commands, cmd_count, "Fork failed\n");
+	// 	else if (commands[i].pid > 0)
+	// 		waitpid(commands[i].pid, NULL, 0);
+	// 	else
+	// 	{
+	// 		if (commands[i].args)
+	// 			execve(commands[i].path, commands[i].args, envp);
+	// 		else
+	// 			execve(commands[i].path, &commands[i].path, envp);
+	// 	}
+	// 	i++;
+	// }
 }
